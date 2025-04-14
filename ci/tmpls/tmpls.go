f912da9845450c0e943038c28f5e9e3ee3e0e909
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-04-14 19:53:28 +0200

    feat: Add readKeyVal func to templates
    Meant to parse slices of key=value strings and extract the value. Useful
    for reusing build-args
    
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- ci/tmpls/tmpls.go
+++ ci/tmpls/tmpls.go
@@ -2,6 +2,8 @@ package tmpls
 
 import (
 	"encoding/json"
+	"maps"
+	"strings"
 	"text/template"
 	"time"
 )
@@ -16,10 +18,26 @@ func jsonMarshal(a any) string {
 	return string(b)
 }
 
+// readKeyVal reads key-value pairs from a slice of strings and returns the value for the given key.
+func readKeyVal(k string, s []string) string {
+	vals := maps.Collect(func(yield func(string, string) bool) {
+		for _, v := range s {
+			k, v1, _ := strings.Cut(v, "=")
+			if !yield(k, v1) {
+				return
+			}
+		}
+	})
+
+	v, _ := vals[k]
+	return v
+}
+
 // TmplFuncs returns a template.FuncMap with functions used in tag templates.
 func TmplFuncs() template.FuncMap {
 	return template.FuncMap{
-		"nowTag": nowTag,
-		"json":   jsonMarshal,
+		"nowTag":     nowTag,
+		"json":       jsonMarshal,
+		"readKeyVal": readKeyVal,
 	}
 }

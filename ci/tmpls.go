c3af6c71184c2c75ecd540c10a3b9cbaf5054499
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-04-14 13:23:53 +0200

    feat: Add json marshaler func to go templates
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- ci/tmpls.go
+++ ci/tmpls.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"text/template"
 	"time"
 )
 
 // TagTmplFuncs returns a template.FuncMap with functions used in tag templates.
 func TagTmplFuncs() template.FuncMap {
-	return template.FuncMap{"now": func() string {
-		return time.Now().UTC().Format("20060102")
-	},
+	return template.FuncMap{
+
+		"now": func() string {
+			return time.Now().UTC().Format("20060102")
+		},
+
+		"json": func(a any) string {
+			b, _ := json.Marshal(a)
+
+			return string(b)
+		},
 	}
 }

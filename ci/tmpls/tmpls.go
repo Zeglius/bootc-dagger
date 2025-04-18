e6a48a6719863711894761e7805e62337bb7dd29
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-04-19 00:07:41 +0200

    feat: Add even more template funcs
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- ci/tmpls/tmpls.go
+++ ci/tmpls/tmpls.go
@@ -37,59 +37,60 @@ var TmplFuncs = template.FuncMap{
 		return regexp.MustCompile(pttrn).ReplaceAllString(old, new)
 	},
 
-	"replace": func(s, old, new string) string {
-		return strings.ReplaceAll(s, old, new)
-	},
-
 	"sort": func(s ...string) []string {
 		sort.Strings(s)
 		return s
 	},
 
-	"parseDockerRef": func(ref string) struct {
-		domain string
-		owner  string
-		image  string
-		tag    string
-	} {
-		// ghcr.io/ublue-os/bazzite:latest -> $domain/$owner/$image:$tag
+	"contains":   strings.Contains,
+	"endsWith":   strings.HasSuffix,
+	"replace":    strings.ReplaceAll,
+	"startsWith": strings.HasPrefix,
 
-		itemRe := `[^/]+`
+	// "parseDockerRef": func(ref string) struct {
+	// 	domain string
+	// 	owner  string
+	// 	image  string
+	// 	tag    string
+	// } {
+	// 	// ghcr.io/ublue-os/bazzite:latest -> $domain/$owner/$image:$tag
 
-		domainOwnerImageRe := regexp.MustCompile(`^` + itemRe + `/` + itemRe + `/` + itemRe)
-		ownerImageRe := regexp.MustCompile(`^` + itemRe + `/` + itemRe)
-		imageRe := regexp.MustCompile(`^` + itemRe)
+	// 	itemRe := `[^/]+`
 
-		res := struct {
-			domain string
-			owner  string
-			image  string
-			tag    string
-		}{}
+	// 	domainOwnerImageRe := regexp.MustCompile(`^` + itemRe + `/` + itemRe + `/` + itemRe)
+	// 	ownerImageRe := regexp.MustCompile(`^` + itemRe + `/` + itemRe)
+	// 	imageRe := regexp.MustCompile(`^` + itemRe)
 
-		switch {
-		case domainOwnerImageRe.MatchString(ref):
-			a := domainOwnerImageRe.FindStringSubmatch(ref)[:3]
-			res.domain, res.owner, res.image = a[1], a[2], a[3]
-		case ownerImageRe.MatchString(ref):
-			a := ownerImageRe.FindStringSubmatch(ref)[:2]
-			res.owner, res.image = a[1], a[2]
-		case imageRe.MatchString(ref):
-			res.image = imageRe.FindString(ref)
-		default:
-			return struct {
-				domain string
-				owner  string
-				image  string
-				tag    string
-			}{}
-		}
+	// 	res := struct {
+	// 		domain string
+	// 		owner  string
+	// 		image  string
+	// 		tag    string
+	// 	}{}
 
-		// extract tag
-		if m := regexp.MustCompile(`:[^:]+$`).FindAllString(ref, 1); len(m) == 1 {
-			res.tag = m[0][1:]
-		}
+	// 	switch {
+	// 	case domainOwnerImageRe.MatchString(ref):
+	// 		a := domainOwnerImageRe.FindStringSubmatch(ref)[:3]
+	// 		res.domain, res.owner, res.image = a[1], a[2], a[3]
+	// 	case ownerImageRe.MatchString(ref):
+	// 		a := ownerImageRe.FindStringSubmatch(ref)[:2]
+	// 		res.owner, res.image = a[1], a[2]
+	// 	case imageRe.MatchString(ref):
+	// 		res.image = imageRe.FindString(ref)
+	// 	default:
+	// 		return struct {
+	// 			domain string
+	// 			owner  string
+	// 			image  string
+	// 			tag    string
+	// 		}{}
+	// 	}
 
-		return res
-	},
+	// 	// extract tag
+	// 	if m := regexp.MustCompile(`:[^:]+$`).FindAllString(ref, 1); len(m) == 1 {
+	// 		res.tag = m[0][1:]
+	// 	}
+
+	// 	return res
+	// },
 }
